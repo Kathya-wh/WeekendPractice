7473ed94d4b46a539e8150c77de971a55ad3fcaa
Kathya <kathyawhite00@gmail.com>
2020-06-28 10:02:32 -0500

    Completed TestMaker

---
+++ WeekendPractice/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace WeekendPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("We would like to verify you are a human before you enter our platform");
+            Console.WriteLine("We will ask you a question that only a human can answer and not a robot, as you can tell we intentionally made a mistake only a human can spot!");
+
+            var theQuestion = "What number is this? (trhee) write the actual number:";
+            var theAnswer = "3";
+            
+
+            var firstQuestion = new TestMaker(theQuestion, theAnswer);
+
+            firstQuestion.Asking();
+
+
+            var userInput = Console.ReadLine();
+
+            if (userInput == firstQuestion.Answer)
+            {
+                Console.WriteLine("That's correct! Verified");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, try again");
+            }
+
+
+         
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ WeekendPractice/TestMaker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeekendPractice
+{
+    class TestMaker
+    {
+        public TestMaker(string question, string answer)
+        {
+            Question = question;
+            Answer = answer;
+        }
+        public string Question { get; set; }
+
+        public string Answer { get; set; }
+
+        public void Asking()
+        {
+            Console.WriteLine($"Please answer the question, {Question}");
+        }
+
+
+    }
+
+}
